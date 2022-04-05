543e20be9edb8313440233d7c36f6f518fad73d7
bgoonz <bryan.guner@gmail.com>
2022-04-05 15:28:02 +0000

    -update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -158,7 +158,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "406783d7565251cc"; }
+/******/ 		__webpack_require__.h = function() { return "ecebc5d9f0ffd515"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ makefile
@@ -0,0 +1,6 @@
+git:
+	git add .
+	git commit -m "$m-update"
+	git push -u origin main
+# just type make into the terminal to exicute these commands!
+
