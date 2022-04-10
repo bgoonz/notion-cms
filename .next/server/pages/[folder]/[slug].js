39d5caa6f9475354806cd7fd753783b6bcdce3ba
Bryan C Guner <bryan.guner@gmail.com>
2022-04-10 05:52:13 +0000

    cache

---
+++ .next/server/pages/[folder]/[slug].js
@@ -0,0 +1,311 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 194;
+exports.ids = [194];
+exports.modules = {
+
+/***/ 5551:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ Change),
+/* harmony export */   "getStaticProps": () => (/* binding */ getStaticProps),
+/* harmony export */   "getStaticPaths": () => (/* binding */ getStaticPaths)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(6689);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _components_layout__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(3671);
+/* harmony import */ var _components_postView__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(2696);
+/* harmony import */ var _components_sidebar__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(3859);
+/* harmony import */ var _lib__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(2575);
+
+
+
+
+
+
+
+function Change({ post  }) {
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(_components_layout__WEBPACK_IMPORTED_MODULE_2__/* ["default"] */ .Z, {
+        title: post.title,
+        children: [
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_components_sidebar__WEBPACK_IMPORTED_MODULE_4__/* ["default"] */ .Z, {
+                posts: [],
+                showBackHome: true
+            }),
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                className: "md:col-span-3",
+                children: /*#__PURE__*/ (0,react__WEBPACK_IMPORTED_MODULE_1__.createElement)(_components_postView__WEBPACK_IMPORTED_MODULE_3__/* ["default"] */ .Z, {
+                    ...post,
+                    key: post.slug
+                })
+            })
+        ]
+    });
+};
+async function getStaticProps({ params: { folder , slug  }  }) {
+    const source = (0,_lib__WEBPACK_IMPORTED_MODULE_5__/* ["default"] */ .Z)();
+    const post = await source.getPost(`/${folder}/${slug}`);
+    return {
+        props: {
+            post: post
+        }
+    };
+}
+async function getStaticPaths() {
+    const source = (0,_lib__WEBPACK_IMPORTED_MODULE_5__/* ["default"] */ .Z)();
+    const posts = await source.getAllPosts();
+    return {
+        paths: posts.map((post)=>({
+                params: {
+                    folder: post.path.slice(1),
+                    slug: post.slug
+                }
+            })
+        ),
+        fallback: false
+    };
+}
+
+
+/***/ }),
+
+/***/ 1635:
+/***/ ((module) => {
+
+module.exports = require("dayjs");
+
+/***/ }),
+
+/***/ 8076:
+/***/ ((module) => {
+
+module.exports = require("gray-matter");
+
+/***/ }),
+
+/***/ 1904:
+/***/ ((module) => {
+
+module.exports = require("markdown-to-jsx");
+
+/***/ }),
+
+/***/ 562:
+/***/ ((module) => {
+
+module.exports = require("next/dist/server/denormalize-page-path.js");
+
+/***/ }),
+
+/***/ 4957:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/head.js");
+
+/***/ }),
+
+/***/ 3539:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/i18n/detect-domain-locale.js");
+
+/***/ }),
+
+/***/ 4014:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/i18n/normalize-locale-path.js");
+
+/***/ }),
+
+/***/ 744:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/image-config-context.js");
+
+/***/ }),
+
+/***/ 5843:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/image-config.js");
+
+/***/ }),
+
+/***/ 8524:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/is-plain-object.js");
+
+/***/ }),
+
+/***/ 8020:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/mitt.js");
+
+/***/ }),
+
+/***/ 4964:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router-context.js");
+
+/***/ }),
+
+/***/ 3938:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/format-url.js");
+
+/***/ }),
+
+/***/ 9565:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/get-asset-path-from-route.js");
+
+/***/ }),
+
+/***/ 1428:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/is-dynamic.js");
+
+/***/ }),
+
+/***/ 1292:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/parse-relative-url.js");
+
+/***/ }),
+
+/***/ 979:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/querystring.js");
+
+/***/ }),
+
+/***/ 6052:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/resolve-rewrites.js");
+
+/***/ }),
+
+/***/ 4226:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/route-matcher.js");
+
+/***/ }),
+
+/***/ 5052:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/route-regex.js");
+
+/***/ }),
+
+/***/ 4241:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/routing-items.js");
+
+/***/ }),
+
+/***/ 9232:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/utils.js");
+
+/***/ }),
+
+/***/ 968:
+/***/ ((module) => {
+
+module.exports = require("next/head");
+
+/***/ }),
+
+/***/ 4780:
+/***/ ((module) => {
+
+module.exports = require("next/script");
+
+/***/ }),
+
+/***/ 6689:
+/***/ ((module) => {
+
+module.exports = require("react");
+
+/***/ }),
+
+/***/ 2293:
+/***/ ((module) => {
+
+module.exports = require("react-katex");
+
+/***/ }),
+
+/***/ 799:
+/***/ ((module) => {
+
+module.exports = require("react-notion");
+
+/***/ }),
+
+/***/ 727:
+/***/ ((module) => {
+
+module.exports = require("react-syntax-highlighter");
+
+/***/ }),
+
+/***/ 7079:
+/***/ ((module) => {
+
+module.exports = require("react-syntax-highlighter/dist/cjs/styles/hljs");
+
+/***/ }),
+
+/***/ 997:
+/***/ ((module) => {
+
+module.exports = require("react/jsx-runtime");
+
+/***/ }),
+
+/***/ 7147:
+/***/ ((module) => {
+
+module.exports = require("fs");
+
+/***/ }),
+
+/***/ 1017:
+/***/ ((module) => {
+
+module.exports = require("path");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [895,61,26], () => (__webpack_exec__(5551)));
+module.exports = __webpack_exports__;
+
+})();
